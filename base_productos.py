f98be174e3d2bd3e0882fb0b4e331dba3e641d50
RaulGrzona <raulgrzona@yahoo.com>
2023-06-02 15:12:39 -0300

    Se creo base_productos.py

---
+++ base_productos.py
@@ -0,0 +1,6 @@
+class Productos:
+    def _init_(self, cod_producto, producto, precio, medida):
+        self.cod_producto = cod_producto
+        self.producto = producto
+        self.precio   = precio
+        self.medida   = medida
